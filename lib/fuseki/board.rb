7dcc555fb7fd17d7b93c07b7548521372e84c35a
Plamen Stoev <psstoev@gmail.com>
2012-02-18 00:17:01 +0200

    Add method to the Board class.

--- lib/fuseki/board.rb
+++ lib/fuseki/board.rb
@@ -1,2 +1,35 @@
 class Board
+  NEIGHBOURS_COORDS = [[-1, 0], [0, -1], [1, 0], [0, 1]].freeze
+
+  def initialize(size)
+    @size = size
+    @cells = {}
+    make_cells
+    @cells.each { |coords, cell| introduce_neighbours coords, cell }
+  end
+
+  def [](x, y)
+    @cells[[x, y]]
+  end
+
+  def include?(x, y)
+    !@cells[[x, y]].nil?
+  end
+
+  private
+
+  def make_cells
+    (0...@size).to_a.product((0...@size).to_a).each do |coords|
+      @cells[[*coords]] = Cell.new
+    end
+  end
+
+  def introduce_neighbours(coords, cell)
+    NEIGHBOURS_COORDS.each do |dcoords|
+      x, y = coords[0] + dcoords[0], coords[1] + dcoords[1]
+      if (0...@size).include?(x) && (0...@size).include?(y)
+        cell.add_neighbour @cells[coords]
+      end
+    end
+  end
 end
--- spec/fuseki/board_spec.rb
+++ spec/fuseki/board_spec.rb
@@ -1,4 +1,33 @@
 require 'spec_helper'
 
 describe Board do
+  it 'can create board with various dimensions' do
+    board = Board.new 3
+  end
+
+  it "can be indexed to retrieve its' cells" do
+    board = Board.new 2
+    board[0, 1].occupy :black
+
+    board[0, 0].empty?.should be_true
+    board[0, 1].empty?.should be_false
+  end
+
+  it 'can tell if it contains a cell with specified coordinates' do
+    board = Board.new 2
+
+    board.include?(0, 0).should be_true
+    board.include?(0, 2).should be_false
+    board.include?(-1, 0).should be_false
+  end
+
+  it "initializes its' cells properly" do
+    board = Board.new 3
+
+    board[0, 0].neighbours.size.should == 2
+    board[0, 1].neighbours.size.should == 3
+    board[1, 1].neighbours.size.should == 4
+
+    board[0, 0].neighbours.map { |n| n.object_id } =~ [board[0, 1].object_id, board[1, 0].object_id]
+  end
 end
