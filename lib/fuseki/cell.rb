f29856429aea4ce5ba609cc733fcb1cfbdfdea3e
Plamen Stoev <psstoev@gmail.com>
2012-02-17 23:45:34 +0200

    Add methods to the Cell class.

--- lib/fuseki/cell.rb
+++ lib/fuseki/cell.rb
@@ -1,2 +1,23 @@
 class Cell
+  attr_reader :player, :neighbours
+
+  def initialize
+    @neighbours = []
+  end
+
+  def occupy(player)
+    @player = player
+  end
+
+  def empty?
+    @player.nil?
+  end
+
+  def unoccupy
+    @player = nil
+  end
+
+  def add_neighbour(other)
+    @neighbours << other
+  end
 end
--- spec/fuseki/cell_spec.rb
+++ spec/fuseki/cell_spec.rb
@@ -4,4 +4,50 @@ describe Cell do
   it 'creates a new cell' do
     Cell.new
   end
+
+  it 'can be occupied by a player' do
+    cell = Cell.new
+    cell.occupy :black
+  end
+
+  it "can tell if it's empty" do
+    cell = Cell.new
+    cell.empty?.should be_true
+
+    cell.occupy :white
+    cell.empty?.should be_false
+  end
+
+  it 'can be unoccupied' do
+    cell = Cell.new
+
+    cell.occupy :black
+    cell.empty?.should be_false
+
+    cell.unoccupy
+    cell.empty?.should be_true
+  end
+
+  it 'tells which player has stone in it' do
+    cell = Cell.new
+
+    cell.occupy :black
+    cell.player.should == :black
+  end
+
+  it "can store information about its' neighbours" do
+    cell1 = Cell.new
+    cell1.add_neighbour Cell.new
+  end
+
+  it "can show information about its' neighbours" do
+    cell1 = Cell.new
+    cell2 = Cell.new
+
+    cell1.add_neighbour cell2
+    cell2.add_neighbour cell1
+
+    cell1.neighbours.size.should == 1
+    cell2.neighbours.size.should == 1
+  end
 end
