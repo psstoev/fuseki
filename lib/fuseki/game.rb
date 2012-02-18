4c41ace4028904815eef14838a899dbd4b7ce165
Plamen Stoev <psstoev@gmail.com>
2012-02-18 06:48:23 +0200

    Add methods to the Game class.

--- lib/fuseki/game.rb
+++ lib/fuseki/game.rb
@@ -1,2 +1,35 @@
 class Game
+  DEFAULT_SIZE = 5
+
+  attr_accessor :player, :board
+
+  def initialize(opts = {})
+    @board = Board.new(opts[:board] || DEFAULT_SIZE)
+    @player = :black
+    @moves = []
+    next_move
+  end
+
+  def move(x, y)
+    if @moves[-1].make_move(x, y)
+      change_player
+      next_move
+    end
+  end
+
+  def cells
+    Hash.new(@board.cells.map do |coords, cell|
+      [coords, cell.player]
+    end.flatten(1))
+  end
+
+  private
+
+  def change_player
+    @player = (@player == :black) ? :white : :black
+  end
+
+  def next_move
+    @moves << Move.new(@player, @board, @moves[-1])
+  end
 end
--- spec/fuseki/game_spec.rb
+++ spec/fuseki/game_spec.rb
@@ -1,4 +1,34 @@
 require 'spec_helper'
 
 describe Game do
+  it 'starts a new game' do
+    Game.new board: 3
+    Game.new
+  end
+
+  it 'shows whose turn it is' do
+    game = Game.new
+    game.player.should == :black
+  end
+
+  it 'makes moves' do
+    game = Game.new
+    game.move(0, 0)
+
+    game.player.should == :white
+    game.board[0, 0].player.should == :black
+  end
+
+  it 'shows the cells' do
+    game = Game.new board: 2
+    game.cells.should == {}
+  end
+
+  it 'does not mess the moves' do
+    game = Game.new
+    game.move(0, 0)
+    game.move(0, 0)
+
+    game.player.should == :white
+  end
 end
